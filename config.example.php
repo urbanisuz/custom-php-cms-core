877265b9eb41a663056312dc4495133e5b526d82
urbanisuz <110439656+urbanisuz@users.noreply.github.com>
2026-09-06 11:44:16 +0500

    Create config.example.php

---
+++ config.example.php
@@ -0,0 +1,20 @@
+<?php
+// config.example.php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => '',
+        'username' => '',
+        'password' => '',
+        'charset' => 'utf8mb4',
+    ],
+    'app' => [
+        'base_url' => 'http://localhost',
+        'default_language' => 'ru',
+        'supported_languages' => ['ru', 'en'],
+        'language_id' => 1,
+        'store_id' => 0,
+        'theme' => 'default',
+    ]
+];
